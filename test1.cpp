46835b60f653719849fd066a76c1159346814ea3
etube-code <english.tube@yandex.com>
2025-01-02 19:40:22 +0300

    added

---
+++ test1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+int main(){
+
+int i;
+while(1){
+std::cout << std::endl;
+for(i=0; i<10; i++){
+std::cout << "a";
+std::cout << std::endl;
+system("clear");
+}
+}
+return 0;
+}
